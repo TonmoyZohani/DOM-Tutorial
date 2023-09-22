237e6071374d635175319b858294fbffc10de2b0
tonmoy-zohani <tonmoyzohani@gmail.com>
2023-09-22 19:54:09 +0600

    static design

---
+++ script.js
@@ -0,0 +1,25 @@
+// function addTask() {
+//   const taskInput = document.getElementById("task");
+//   const taskText = taskInput.value.trim();
+
+//   if (taskText !== "") {
+//     const taskList = document.getElementById("task-list");
+//     const li = document.createElement("li");
+//     li.innerHTML = `
+//       ${taskText}
+//       <button onclick="removeTask(this)">Remove</button>
+//     `;
+//     taskList.appendChild(li);
+//     taskInput.value = "";
+//   }
+// }
+
+// function removeTask(button) {
+//   console.log(button.parentElement);
+//   const taskList = document.getElementById("task-list");
+//   taskList.removeChild(button.parentElement);
+// }
+
+function addTask(){
+    
+}
